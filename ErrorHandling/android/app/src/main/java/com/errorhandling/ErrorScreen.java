d367087668af6ef4f2d53c62115a2082d587a731
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-07-14 20:39:06 +0900

    AndroidのNative Modulesを一旦追加

---
+++ ErrorHandling/android/app/src/main/java/com/errorhandling/ErrorScreen.java
@@ -0,0 +1,12 @@
+package com.errorhandling;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ErrorScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_screen);
+    }
+}
--- ErrorHandling/src/App.tsx
+++ ErrorHandling/src/App.tsx
@@ -1,9 +1,10 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackNav} from 'navigation';
 import React from 'react';
-import {Platform} from 'react-native';
+import {Platform, NativeModules} from 'react-native';
 
 import {ErrorBoundary} from './components/ErrorBoundary';
+const {ExceptionHandlerModule} = NativeModules;
 
 if (Platform.OS !== 'web') {
   const globalHandler = ErrorUtils.getGlobalHandler();
@@ -13,25 +14,22 @@ if (Platform.OS !== 'web') {
     // TODO Firebase Crashlyticsにログ送信
     console.log('Did catch error In ErrorUtils.');
 
-    if (__DEV__) {
-      // Releaseモードでデフォルトのグローバルハンドラを呼び出すと、アプリクラッシュするためDebugモードのみ呼び出す
-
-      // デフォルトのグローバルハンドラは恐らく↓
-      // https://github.com/facebook/react-native/blob/dc80b2dcb52fadec6a573a9dd1824393f8c29fdc/Libraries/Core/setUpErrorHandling.js#L32
-      globalHandler(error, isFatal);
-    }
+    // デフォルトのグローバルハンドラは恐らく↓
+    // https://github.com/facebook/react-native/blob/dc80b2dcb52fadec6a573a9dd1824393f8c29fdc/Libraries/Core/setUpErrorHandling.js#L32
+    globalHandler(error, isFatal);
 
     // TODO アプリ再起動のUI表示。再起動はNative側から再起動するかJSCの再起動（バンドルモジュールのリロード）のどちらにするかは要検討
     console.log('Reload app.');
   });
 }
+ExceptionHandlerModule.handle();
 
 export const App = () => {
   return (
-    <ErrorBoundary>
-      <NavigationContainer>
-        <RootStackNav />
-      </NavigationContainer>
-    </ErrorBoundary>
+    // <ErrorBoundary>
+    <NavigationContainer>
+      <RootStackNav />
+    </NavigationContainer>
+    // </ErrorBoundary>
   );
 };
