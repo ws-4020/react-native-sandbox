b2fe57eb9e562c1a0843dc332157ce90a63e4b7b
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-08-05 13:58:07 +0900

    sleep処理を追加

--- ErrorHandling/.script/http-server.js
+++ ErrorHandling/.script/http-server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
     const path = req.url;
     const parsed = parseInt(path.replace('/', ''));
     const statusCode = isNaN(parsed) ? 200 : parsed;
+
+    // タイムアウトの検証でSleepしたい場合はコメントを解除してください
+    // await new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve();
+    //   }, 1200000);
+    // });
+
     res.writeHead(statusCode, {'Content-Type': 'text/html'});
     res.end(
       `<html lang="ja"><meta charset="utf-8"/><body>ステータスコードは${statusCode}です。<br><img src="http://localhost:3021" alt="img1"><img src="http://localhost:3000/400" alt="img2"></body></html>`,
